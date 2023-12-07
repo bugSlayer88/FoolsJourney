92d01d11bb8341e56a7b45b0e9f4cc57b6cf0463
bugSlayer88 <rileyfranklincg@gmail.com>
2023-12-07 14:48:29 -0700

    went back to original create deck method

--- tarot_spreads/simple_three_input.py
+++ tarot_spreads/simple_three_input.py
@@ -1,10 +1,10 @@
 import random
 import create_deck
 
-import deck_redo
+import create_tarot_deck
 
-# deck = create_deck.Deck()
-deck = deck_redo.TarotDeck()
+deck = create_deck.Deck()
+# deck = deck_redo.TarotDeck()
 # deck.shuffle()
 
 
